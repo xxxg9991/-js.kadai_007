352c1c48453204e6811b16669e47a9a3ee5ca84a
xxxg9991 <153571560+xxxg9991@users.noreply.github.com>
2024-01-27 19:12:00 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,18 +1,22 @@
 let num = 15;
 
-console.log
+console.log;
 
 // 変数numの値が３と５の倍数の場合、"３と５の倍数です”と出力する
 if (num % 3 === 0 && num % 5 === 0) {
   console.log("３と５の倍数です")
   }
   
+  
 // 変数numの値が３の倍数の場合、"３の倍数です"と出力する
 else if (num % 3 === 0) {
+  console.log("３の倍数です")
 }
+
 
 // 変数numの値が５の倍数の場合、"５の倍数です"と出力する
 else if (num % 5 === 0) {
+  console.log("５の倍数です")
 }
 
 
